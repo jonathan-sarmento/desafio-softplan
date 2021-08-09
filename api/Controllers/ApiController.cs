859c4376b7ddc7367b8f030f56d0775bf7dfe45a
Jonathan Sousa <83629592+jonathan-sarmento@users.noreply.github.com>
2021-08-09 01:03:44 -0400

    Add files via upload

---
+++ api/Controllers/ApiController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Controllers
+{
+    
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/taxaJuros")]
+        public double Get()
+        {
+            return 0.01;
+        }
+    }
+}
